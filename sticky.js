b12e90adfd7e15db137d62d2d8b3a368e25b425f
James EJ <james@makersacademy.com>
2013-11-05 02:18:08 +0000

    Updates width on resize

--- sticky.js
+++ sticky.js
@@ -20,33 +20,47 @@ angular.module('sticky', []).directive("sticky", function($window) {
 
       var $win = angular.element($window);
 
+      var stickyItem = function(item){
+        item.element.attr("style","position:fixed;top:"+top_offset+"px;left:"+(item.left-left_offset)+"px;width:"+item.width+"px;");
+        item.element.addClass("stuck");
+        item.isStuck = true;
+      };
+      var unStickyItem = function(item){
+        item.element.removeClass("stuck");
+        item.element.attr("style","");
+        item.isStuck = false;
+      };
+
       if (scope._stickyElements === undefined) {
         scope._stickyElements = [];
 
         $win.bind("scroll", function(e) {
           var pos = windowScrollTop();
           for (var i=0; i<scope._stickyElements.length; i++) {
-
             var item = scope._stickyElements[i];
-
             if (!item.isStuck && pos > item.start) {
-              item.element.attr("style","position:fixed;top:"+top_offset+"px;left:"+(item.left-left_offset)+"px;width:"+item.width+"px;");
-              item.element.addClass("stuck");
-              item.isStuck = true;
+              stickyItem(item);
             }
             else if (item.isStuck && pos < item.start) {
-              item.element.removeClass("stuck");
-              item.element.attr("style","");
-              item.isStuck = false;
+              unStickyItem(item);
             }
           }
         });
 
+        var updateItem = function(item){
+          item.start = offsetTop(item.element)-top_offset,
+          item.width = item.element[0].clientWidth,
+          item.left = item.element[0].getBoundingClientRect().left
+        }
         var recheckPositions = function() {
           for (var i=0; i<scope._stickyElements.length; i++) {
             var item = scope._stickyElements[i];
             if (!item.isStuck) {
-              item.start = offsetTop(item.element)-top_offset;
+              updateItem(item);
+            } else {
+              unStickyItem(item);
+              updateItem(item);
+              stickyItem(item);
             }
           }
         };
@@ -56,11 +70,9 @@ angular.module('sticky', []).directive("sticky", function($window) {
 
       var item = {
         element: element,
-        isStuck: false,
-        start: offsetTop(element)-top_offset,
-        width: element[0].clientWidth,
-        left: item.element[0].getBoundingClientRect().left
+        isStuck: false
       };
+      updateItem(item);
 
       scope._stickyElements.push(item);
 
